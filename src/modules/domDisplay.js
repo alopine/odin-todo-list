deb5fdbd8369b99d1d01c079aa49479b24e51ecf
alopine <loariel98@gmail.com>
2022-04-26 20:40:33 -0700

    Add display mockup draft and additional stylings

--- src/modules/domDisplay.js
+++ src/modules/domDisplay.js
@@ -1,1 +1,8 @@
 // Controls visual elements of DOM
+
+// Select DOM elements
+const main = document.querySelector("main");
+
+// Function to create sidebar list of projects
+
+// Function to create 

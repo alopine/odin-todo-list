b7d012ecf0da4586add3db89c858073a6b31c07d
alopine <loariel98@gmail.com>
2022-05-14 15:03:54 -0700

    Fix todo storage bug with complete status

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -18,6 +18,7 @@ export default class storage {
             // Loop to iterate over each project's todos
             project._projectTodos.forEach((todo) => {
                 let tempTodo = new Todo(todo._title, todo._description, todo._dueDate, todo._priority);
+                tempTodo.complete = todo._complete;
                 tempTodo.id = todo._id;
                 tempProject.projectTodos.push(tempTodo);
                 console.log(tempTodo);
